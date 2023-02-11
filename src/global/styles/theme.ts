3e665623f86cf194f6161e173713c545044a65c7
Albert Dias <albert.uft@gmail.com>
2023-02-10 23:09:39 -0300

    [feat] criação theme, instalacao libs auxiliares

---
+++ src/global/styles/theme.ts
@@ -0,0 +1,27 @@
+export default {
+    colors: {
+        primary: '#5636D3',
+
+        secundary: '#FF872C',
+        secundary_D: '#FCB179',
+
+        success: '#12A454',
+        success_light: 'rgba(18,164,84,0.5)',
+
+        attention: '#eb3f5b',
+        attention_light: 'rgba(232,63,91,0.5)',
+
+        shape: '#fff',
+        background: '#F0F2F5',
+        place: '#969CB2',
+        text: '#363F5F',
+        textbold: '#000',
+    },
+    fonts: {
+        light: "Roboto_300Light",
+        regular: "Roboto_400Regular",
+        mediun: "Roboto_500Medium",
+        bold: "Roboto_700Bold",
+        black: "Roboto_900Black_Italic"
+    }
+}
